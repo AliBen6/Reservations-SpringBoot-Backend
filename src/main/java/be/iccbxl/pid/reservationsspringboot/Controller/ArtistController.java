5faf120b6d204b92f70e3572964a780b2ee05284
abenaiche <ali.benaiche@contraste.com>
2023-03-02 16:23:05 +0100

    change api url route

--- src/main/java/be/iccbxl/pid/reservationsspringboot/Controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/Controller/ArtistController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("api/V1/artists")
+@RequestMapping("/api/V1/artists")
 @Validated
 public class ArtistController {
 
